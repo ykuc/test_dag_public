4f348aa10e12b80ce53ab37a5fe5e77144c29769
ykuc <140019825+ykuc@users.noreply.github.com>
2023-08-12 00:25:02 +0300

    Update test_dag.py

--- test/dags/test_dag.py
+++ test/dags/test_dag.py
@@ -1,11 +1,11 @@
- import datetime
+import datetime
 
- from airflow import DAG
- from airflow.operators.empty import EmptyOperator
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
 
- with DAG(
+with DAG(
      dag_id="my_dag_name",
      start_date=datetime.datetime(2021, 1, 1),
      schedule="@daily",
- ):
-     EmptyOperator(task_id="task")
+):
+    EmptyOperator(task_id="task")
