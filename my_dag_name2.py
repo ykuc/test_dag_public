1c6922fa80df86584c80c0fa855f9dad2968461c
ykuc <140019825+ykuc@users.noreply.github.com>
2023-08-12 00:24:18 +0300

    Update my_dag_name2.py

--- my_dag_name2.py
+++ my_dag_name2.py
@@ -1,11 +1,22 @@
- import datetime
+from airflow.decorators import dag, task
+from airflow.operators.bash import BashOperator
 
- from airflow import DAG
- from airflow.operators.empty import EmptyOperator
+from datetime import datetime
 
- with DAG(
-     dag_id="my_dag_name2",
-     start_date=datetime.datetime(2021, 1, 1),
-     schedule="@daily",
- ):
-     EmptyOperator(task_id="task")
+@dag(start_date=datetime(2023, 1 , 1), schedule='@daily', catchup=False)
+def parallel_dag():
+
+    tasks = [BashOperator(task_id='task_{0}'.format(t), bash_command='sleep 60'.format(t)) for t in range(1, 4)]
+
+    @task
+    def task_4(data):
+        print(data)
+        return 'done'
+    
+    @task
+    def task_5(data):
+        print(data)
+
+    tasks >> task_5(task_4(42))
+
+parallel_dag()
